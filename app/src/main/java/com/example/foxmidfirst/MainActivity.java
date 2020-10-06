de1b8b51534513a44080b0ab896a276837b20f11
Roman Leizerovych (Mentor) <romariomkk@gmail.com>
2020-10-06 13:24:04 +0300

    Revert "Merge branch 'dev' into 'master'"
    This reverts merge request !1

--- app/src/main/java/com/example/foxmidfirst/MainActivity.java
+++ app/src/main/java/com/example/foxmidfirst/MainActivity.java
@@ -3,50 +3,12 @@ package com.example.foxmidfirst;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private EditText editTextUserString;
-    private EditText editTextUserSymbolExclude;
-    private TextView textViewAnswer;
-    private Button btnStart;
-    Rotate rotate = new Rotate();
-    private String userStringSentence;
-    private String[] myUserArrString;
-
-    private String userSymboIgnore;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
-
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                userSymboIgnore = editTextUserSymbolExclude.getText().toString();//stringa iz edit text
-                userStringSentence = editTextUserString.getText().toString();
-
-                textViewAnswer.setText(rotate.rotateWord(userStringSentence,userSymboIgnore));
-            }
-        });
-
-    }
-
-    private void init() {
-        editTextUserString = findViewById(R.id.edText1);
-        editTextUserSymbolExclude = findViewById(R.id.edText2);
-        textViewAnswer = findViewById(R.id.textView4);
-        btnStart = findViewById(R.id.button1);
-        userSymboIgnore = editTextUserSymbolExclude.getText().toString();
-        userStringSentence = editTextUserString.getText().toString();
-
     }
 }
