c99c7cbd1dcf4ee29841665ed3a98519623f35b7
Eugeniia <mrs.trofan@gmail.com>
2020-10-12 03:37:32 +0300

    Work on errors.3. Everything works=)

--- app/src/main/java/com/example/foxmidfirst/Rotator.java
+++ app/src/main/java/com/example/foxmidfirst/Rotator.java
@@ -6,59 +6,45 @@ public class Rotator {
 
     public String rotateWord(String sentenceFromUser, String ignoreSymbols) {
         String[] wordsFromIncomingSentence = sentenceFromUser.split(" ");
-        ArrayList<Integer> indexesOfTheIgnoreChars = new ArrayList<>();
         for (int i = 0; i < wordsFromIncomingSentence.length; i++) {
-            String s = wordsFromIncomingSentence[i];
-            for (int j = 0; j < s.length(); j++) {
-                for (int k = 0; k < ignoreSymbols.length(); k++) {
-                    if (s.charAt(j) == ignoreSymbols.charAt(k)) {
-                        indexesOfTheIgnoreChars.add(j);
-                    }
-                }
-            }
-            char[] tempCharArr = new char[s.length()];
-            tempCharArr = s.toCharArray();
+            String oneWordFromUserSentence = wordsFromIncomingSentence[i];
+            char[] tempCharArr = new char[oneWordFromUserSentence.length()];
+            tempCharArr = oneWordFromUserSentence.toCharArray();
             int indFirstLetter = 0;
-            int indLastLetter = s.length() - 1;
+            int indLastLetter = oneWordFromUserSentence.length() - 1;
             char tempChar;
             for (int k = 0; k < tempCharArr.length; k++) {
-                if (thereIsIgnoreChar(indFirstLetter, indexesOfTheIgnoreChars) != true && thereIsIgnoreChar(indLastLetter, indexesOfTheIgnoreChars) != true) {
+                if (thereIsIgnoreChar(oneWordFromUserSentence.charAt(indFirstLetter), ignoreSymbols) != true && thereIsIgnoreChar(oneWordFromUserSentence.charAt(indLastLetter), ignoreSymbols) != true) {
                     tempChar = tempCharArr[indFirstLetter];
                     tempCharArr[indFirstLetter] = tempCharArr[indLastLetter];
                     tempCharArr[indLastLetter] = tempChar;
                     indFirstLetter++;
                     indLastLetter--;
-                } else if (thereIsIgnoreChar(indFirstLetter, indexesOfTheIgnoreChars) == true && thereIsIgnoreChar(indLastLetter, indexesOfTheIgnoreChars) != true) {
+                } else if (thereIsIgnoreChar(oneWordFromUserSentence.charAt(indFirstLetter), ignoreSymbols) == true && thereIsIgnoreChar(oneWordFromUserSentence.charAt(indLastLetter), ignoreSymbols) != true) {
                     indFirstLetter++;
-                } else if (thereIsIgnoreChar(indFirstLetter, indexesOfTheIgnoreChars) != true && thereIsIgnoreChar(indLastLetter, indexesOfTheIgnoreChars) == true) {
+                } else if (thereIsIgnoreChar(oneWordFromUserSentence.charAt(indFirstLetter), ignoreSymbols) != true && thereIsIgnoreChar(oneWordFromUserSentence.charAt(indLastLetter), ignoreSymbols) == true) {
                     indLastLetter--;
                 }
                 if (indFirstLetter >= indLastLetter) {
                     k = tempCharArr.length;
                 }
             }
-             wordsFromIncomingSentence[i] = String.copyValueOf(tempCharArr);
-            indexesOfTheIgnoreChars.removeAll(indexesOfTheIgnoreChars);
+            wordsFromIncomingSentence[i] = String.copyValueOf(tempCharArr);
+            //indexesOfTheIgnoreChars.removeAll(indexesOfTheIgnoreChars);
         }
-        String sentence="";
-        for (int i=0;i<wordsFromIncomingSentence.length;i++){
-            sentence+=wordsFromIncomingSentence[i]+" ";
-
+        String sentence = "";
+        for (int i = 0; i < wordsFromIncomingSentence.length; i++) {
+            sentence += wordsFromIncomingSentence[i] + " ";
         }
         sentence.trim();
-        return  sentence;
+        return sentence;
     }
-    private boolean thereIsIgnoreChar(int indLetter, ArrayList index) {
+
+    private boolean thereIsIgnoreChar(char charFromWord, String ignore) {
         boolean bool = false;
-        for (int i = 0; i < index.size(); i++) {
-            int x = (int) index.get(i);
-            if (x == indLetter) {
-                bool = true;
-            }
+        if (ignore.contains(String.valueOf(charFromWord))) {
+            bool = true;
         }
         return bool;
     }
 }
-
-
-
