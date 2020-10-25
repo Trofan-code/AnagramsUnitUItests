be956bebe5af3d84787c042554952d67fb17f86b
Eugeniia <mrs.trofan@gmail.com>
2020-10-25 15:29:03 +0300

    Revert "tests finished 1.3 Parameterized its cool !=))"
    This reverts commit 5526ec12

--- app/src/androidTest/java/com/example/foxmidfirst/MainActivityUITest.java
+++ app/src/androidTest/java/com/example/foxmidfirst/MainActivityUITest.java
@@ -15,57 +15,43 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import java.util.Arrays;
-import java.util.Collection;
+
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.StringContains.containsString;
-import static org.junit.Assert.assertNotNull;
 
 
-@RunWith(Parameterized.class)
+
+
+@RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MainActivityUITest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
     Rotator rotator;
-    private String userString ="";
-    private String ignoreString ="";
-    public MainActivityUITest(String userString, String ignoreString) {
-        super();
-        this.userString = userString;
-        this.ignoreString = ignoreString;
-    }
     @Before
     public void before() {
         rotator = new Rotator();
     }
-    @Parameterized.Parameters
-    public static Collection input(){
-        return Arrays.asList(new Object[][] {
-                {"Foxminded Cool 24/7","24/7"},
-                {"","24/7"},
-                {null,"24/7"},
-                {"1241241412412",null},
-                {"Foxminded Cool 24/7",""},
-                {"Dfsgyhsdrfhgdfhgbdfghseiudytgfiuseyhgvkjdxhvkusdyrfgiuesayhfoiuhsdkjvhdfkxjgbviserugoisedjglkdfhjbkjhdfcklughbdkfsujghoiklshdgoi",""},
-                {"Dfsgyhsdrfhgdfhgbdfghseiudytgfiuseyhgvkjdxhvkusdyrfgiuesayhfoiuhsdkjvhdfkxjgbviserugoisedjglkdfhjbkjhdfcklughbdkfsujghoiklshdgoi","Dfsgyhsdrfhgdfhgbdfghseiudytgfiuseyhgvkjdxhvkusdyrfgiuesayhfoiuhsdkjvhdfkxjgbviserugoisedjglkdfhjbkjhdfcklughbdkfsujghoiklshdgoi"},
-                });
-    }
     @After
     public void after() {
     }
+
     @Test
     public void edTextTest1() {
+        String userString = "Foxminded Cool 24/7";
+        String ignoreString = "24/7";
         String answer = rotator.rotateWord(userString, ignoreString);
-        assertNotNull(rotator.rotateWord(userString,ignoreString));
         onView(withId(R.id.textView1)).check(matches(isDisplayed()));
         onView(withId(R.id.edText1)).check(matches(isDisplayed()));
         onView(withId(R.id.textView2)).check(matches(isDisplayed()));
@@ -77,5 +63,76 @@ public class MainActivityUITest {
         onView(withId(R.id.edText2)).perform(typeText(ignoreString), closeSoftKeyboard());
         onView(withId(R.id.button1)).perform(click());
         onView(withId(R.id.textView4)).check(matches(withText(containsString(answer))));
+    }
+    @Test
+    public void edTextTest2() {
+        String userString = "";
+        String ignoreString = "24/7";
+        String answer = rotator.rotateWord(userString, ignoreString);
+        onView(withId(R.id.textView1)).check(matches(isDisplayed()));
+        onView(withId(R.id.edText1)).check(matches(isDisplayed()));
+        onView(withId(R.id.textView2)).check(matches(isDisplayed()));
+        onView(withId(R.id.edText2)).check(matches(isDisplayed()));
+        onView(withId(R.id.button1)).check(matches(isDisplayed()));
+        onView(withId(R.id.textView3)).check(matches(isDisplayed()));
+        onView(withId(R.id.textView4)).check(matches(isDisplayed()));
+        onView(withId(R.id.edText1)).perform(typeText(userString), closeSoftKeyboard());
+        onView(withId(R.id.edText2)).perform(typeText(ignoreString), closeSoftKeyboard());
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.textView4)).check(matches(withText(containsString(answer))));
+    }
+    @Test
+    public void edTextTest3() {
+        String userString = "Foxminded Cool 24/7";
+        String ignoreString = "";
+        String answer = rotator.rotateWord(userString, ignoreString);
+        onView(withId(R.id.textView1)).check(matches(isDisplayed()));
+        onView(withId(R.id.edText1)).check(matches(isDisplayed()));
+        onView(withId(R.id.textView2)).check(matches(isDisplayed()));
+        onView(withId(R.id.edText2)).check(matches(isDisplayed()));
+        onView(withId(R.id.button1)).check(matches(isDisplayed()));
+        onView(withId(R.id.textView3)).check(matches(isDisplayed()));
+        onView(withId(R.id.textView4)).check(matches(isDisplayed()));
+        onView(withId(R.id.edText1)).perform(typeText(userString), closeSoftKeyboard());
+        onView(withId(R.id.edText2)).perform(typeText(ignoreString), closeSoftKeyboard());
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.textView4)).check(matches(withText(containsString(answer))));
+
+    }
+    @Test
+    public void edTextTest4() {
+        String userString = "Dfsgyhsdrfhgdfhgbdfghseiudytgfiuseyhgvkjdxhvkusdyrfgiuesayhfoiuhsdkjvhdfkxjgbviserugoisedjglkdfhjbkjhdfcklughbdkfsujghoiklshdgoi";
+        String ignoreString = "";
+        String answer = rotator.rotateWord(userString, ignoreString);
+        onView(withId(R.id.textView1)).check(matches(isDisplayed()));
+        onView(withId(R.id.edText1)).check(matches(isDisplayed()));
+        onView(withId(R.id.textView2)).check(matches(isDisplayed()));
+        onView(withId(R.id.edText2)).check(matches(isDisplayed()));
+        onView(withId(R.id.button1)).check(matches(isDisplayed()));
+        onView(withId(R.id.textView3)).check(matches(isDisplayed()));
+        onView(withId(R.id.textView4)).check(matches(isDisplayed()));
+        onView(withId(R.id.edText1)).perform(typeText(userString), closeSoftKeyboard());
+        onView(withId(R.id.edText2)).perform(typeText(ignoreString), closeSoftKeyboard());
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.textView4)).check(matches(withText(containsString(answer))));
+
+    }
+    @Test
+    public void edTextTest5() {
+        String userString = "Dfsgyhsdrfhgdfhgbdfghseiudytgfiuseyhgvkjdxhvkusdyrfgiuesayhfoiuhsdkjvhdfkxjgbviserugoisedjglkdfhjbkjhdfcklughbdkfsujghoiklshdgoi";
+        String ignoreString = "Dfsgyhsdrfhgdfhgbdfghseiudytgfiuseyhgvkjdxhvkusdyrfgiuesayhfoiuhsdkjvhdfkxjgbviserugoisedjglkdfhjbkjhdfcklughbdkfsujghoiklshdgoi";
+        String answer = rotator.rotateWord(userString, ignoreString);
+        onView(withId(R.id.textView1)).check(matches(isDisplayed()));
+        onView(withId(R.id.edText1)).check(matches(isDisplayed()));
+        onView(withId(R.id.textView2)).check(matches(isDisplayed()));
+        onView(withId(R.id.edText2)).check(matches(isDisplayed()));
+        onView(withId(R.id.button1)).check(matches(isDisplayed()));
+        onView(withId(R.id.textView3)).check(matches(isDisplayed()));
+        onView(withId(R.id.textView4)).check(matches(isDisplayed()));
+        onView(withId(R.id.edText1)).perform(typeText(userString), closeSoftKeyboard());
+        onView(withId(R.id.edText2)).perform(typeText(ignoreString), closeSoftKeyboard());
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.textView4)).check(matches(withText(containsString(answer))));
+
     }
 }
